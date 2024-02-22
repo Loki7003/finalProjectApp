ebdd5763c89f0d0c0ac561275ea4c1337be54aab
kzwierzc <kzwierzc@PL2TWKDS3>
2024-02-22 07:32:23 +0100

    Add project files.

---
+++ finalProjectApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace finalProjectApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
