a23e7fe91c669090acdd1289d17294cafb56cbc1
Loki7003 <80339875+Loki7003@users.noreply.github.com>
2024-02-23 13:27:20 +0100

    Authentication was improved
    Updated authentication to make it more secure and added additional class and model.

---
+++ finalProjectApp/ConnectionClass.cs
@@ -0,0 +1,7 @@
+﻿namespace finalProjectApp
+{
+	public class ConnectionClass
+	{
+		public readonly string ConnectionString = "Data Source=vps-64805245.vps.ovh.net;Initial Catalog=finalProjectDb;User Id=connection_operator;Password=JsMuYtnJ6QwKqE2XXqsM";
+	}
+}
