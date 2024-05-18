cdc19c304483f3295d4c99cebc358f0ede52a3e9
Loki7003 <80339875+Loki7003@users.noreply.github.com>
2024-05-18 13:09:50 +0200

    Session has been added

--- finalProjectApp/Program.cs
+++ finalProjectApp/Program.cs
@@ -3,14 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Adds a default in-memory implementation of IDistributedCache.
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromMinutes(30);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -18,10 +30,27 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Use session
+app.UseSession();
+
+app.Use(async (context, next) =>
+{
+	if (context.Session.GetString("username") != null && context.Session.GetString("userid") != null)
+	{
+		// If the user is logged in (i.e., "username" and "userid" exist in the session), redirect to the User Index page
+		context.Response.Redirect("/User/Index");
+	}
+	else
+	{
+		// If the user is not logged in, just call the next middleware in the pipeline
+		await next.Invoke();
+	}
+});
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
